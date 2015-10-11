d8352b77161c393186a152214602d1a5f5c3146a
Jeffrey Huang <peidong10@hotmail.com>
2015-10-11 11:24:01 -0400

    added new scene

---
+++ unity-client/Assets/Scripts/Main Menu/UIButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButton : MonoBehaviour {
+	
+	public Transition change;
+	public void ChangeLevel(){
+		Debug.Log ("foo");
+		StartCoroutine (Transition());
+	}
+	IEnumerator Transition(){
+		float fadeTime = change.BeginFade (1);
+		yield return new WaitForSeconds(fadeTime);
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+}
